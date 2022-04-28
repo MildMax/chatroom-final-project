c84276e674f70f72ff9fd9f45a558e8385e88387
Shephzibah <98137690+Shephzibah@users.noreply.github.com>
2022-04-28 02:33:42 -0400

    added java docs

--- client/ServerInfo.java
+++ client/ServerInfo.java
@@ -1,24 +1,35 @@
 package client;
 
+/**
+ * Serverinfo class which holds the necessary information required by the 
+ * client.
+ *
+ */
 public class ServerInfo {
 
-    private final String centralHost;
-    private final int centralPort;
-    private final boolean isTest;
+  private final String centralHost;
+  private final int centralPort;
+  private final boolean isTest;
 
-    public ServerInfo(String centralHost, int centralPort, boolean isTest) {
-        this.centralHost = centralHost;
-        this.centralPort = centralPort;
-        this.isTest = isTest;
-    }
+  /**
+   * server infor which is needed for the client.
+   * @param centralHost host name
+   * @param centralPort port number
+   * @param isTest  boolean flag
+   */
+  public ServerInfo(String centralHost, int centralPort, boolean isTest) {
+    this.centralHost = centralHost;
+    this.centralPort = centralPort;
+    this.isTest = isTest;
+  }
 
-    public String getCentralHost() {
-        return centralHost;
-    }
+  public String getCentralHost() {
+    return centralHost;
+  }
 
-    public int getCentralPort() {
-        return centralPort;
-    }
+  public int getCentralPort() {
+    return centralPort;
+  }
 
-    public boolean getIsTest() { return this.isTest; }
+  public boolean getIsTest() { return this.isTest; }
 }
